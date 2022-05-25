5ff574d56961916a5b21d2ed0fb1649705466fc1
jose.raul.navarro.avila27@uabc.edu.mx <jose.raul.navarro.avila27@uabc.edu.mx>
2022-05-24 22:07:01 -0700

    Agregar archivos de proyecto.

---
+++ While/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contraseña1, contraseña2 = "";
+
+            Console.Write("Escriba su contraseña: ");
+            contraseña1 = Console.ReadLine();
+
+            while (contraseña1 != contraseña2)
+            {
+                Console.Write("Confirme su contraseña: ");
+                contraseña2 = Console.ReadLine();
+            }
+            Console.WriteLine("Contraseña almacenada!");
+            Console.ReadKey();
+        }
+    }
+}
